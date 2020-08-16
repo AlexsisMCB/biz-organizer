4474829f19bfa301ad3b4cdcff4c95fb9b11c368
Alexsis Conley Brown <alexsismcb@gmail.com>
2020-08-15 23:13:58 -0500

    flow of questions

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+
 
 const choices = () => {
     return inquirer.prompt([
@@ -16,19 +18,35 @@ const choices = () => {
             'Update an employee role',
             'Quit']
         }
-    ]).then(data => {
+    ])
+    .then(data => {
         if (data.choices === 'View all departments') {
-            console.log('All departments');
+            console.table([
+                {
+                    name: 'Department 1'
+                }
+            ]);
         } else if (data.choices === 'View all roles') {
-            console.log('All roles');
+            console.table([
+                {
+                    name: 'Role 1'
+                }
+            ]);
         } else if (data.choices === 'View all employees') {
-            console.log('All employees');
+            console.table([
+                {
+                    name: 'Employee 1'
+                }
+            ]);
         } else if (data.choices === 'Add a department') {
             console.log('Add department');
+            addDepartment();
         } else if (data.choices === 'Add a role') {
             console.log('Add role');
+            addRole();
         } else if (data.choices === 'Add an employee') {
             console.log('Add employee');
+            addEmployee();
         } else if (data.choices === 'Update an employee role') {
             console.log('Update role');
         } else if (data.choices === 'Quit') {
@@ -36,5 +54,134 @@ const choices = () => {
         }
     });
 };
+
+const addDepartment = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: "What's the name of the department?",
+            validate: departmentNameInput => {
+                if (departmentNameInput) {
+                    return true;
+                } else {
+                    console.log('Please give a name!');
+                    return false;
+                }
+            }
+        }
+    ]).then(data => {
+        console.log(data);
+        choices();
+    });
+}
+
+const addRole = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roleTitle',
+            message: "What's the title of the new role?",
+            validate: roleTitleInput => {
+                if (roleTitleInput) {
+                    return true;
+                } else {
+                    console.log('Please give a title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: "What's the new role salary?",
+            validate: roleSalaryInput => {
+                if (roleSalaryInput) {
+                    return true;
+                } else {
+                    console.log('Salary please!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'roleDepartment',
+            message: 'What department is the the new role in?',
+            validate: roleDepartmentInput => {
+                if (roleDepartmentInput) {
+                    return true;
+                } else {
+                    console.log('Department please!');
+                    return false;
+                }
+            }
+        }
+    ]).then(data => {
+        console.log(data);
+        choices();
+    });
+}
+
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: "What's the first name of the new employee?",
+            validate: firstNameInput => {
+                if (firstNameInput) {
+                    return true;
+                } else {
+                    console.log('Please give a first name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: "What's the last name?",
+            validate: lastNameInput => {
+                if (lastNameInput) {
+                    return true;
+                } else {
+                    console.log('Please give a last name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeRole',
+            message: "What's the role of the employee?",
+            validate: employeeRoleInput => {
+                if (employeeRoleInput) {
+                    return true;
+                } else {
+                    console.log('Role please!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeManager',
+            message: "Who is the manager for this employee?",
+            validate: employeeManagerInput => {
+                if (employeeManagerInput) {
+                    return true;
+                } else {
+                    console.log('Please assign a manager!');
+                    return false;
+                }
+            }
+        }
+    ]).then(data => {
+        console.log(data);
+        choices();
+    });
+};
+
 
 choices();
